90fa0ebe9b9552dd1f05b58acf42a876ac491007
MiiguelM <miguelmarianonovais@gmail.com>
2026-03-21 20:26:23 -0300

    Adiciona programa de soma em C

---
+++ soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, soma;
+    printf("digite dois numeros: ");
+    scanf("%d %d", &a, &b);
+    soma = a + b;
+    printf("soma = %d\n", soma);
+
+    return 0;
+}
+
